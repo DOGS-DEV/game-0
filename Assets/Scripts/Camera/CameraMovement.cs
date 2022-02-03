9e0318e58f77440c52c576da4a8d18f67cc82f29
vegiwoo <vegiwoo@protonmail.com>
2022-02-03 20:54:24 +0300

    Implimentation 2 - Step 02

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game0
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField] 
+
+
+
+        private void Update()
+        {
+            float lookX = Mouse.current.delta.x.ReadValue();
+            //transform.rotation * 
+        }
+
+
+        
+        
+    }
+}
+
+
